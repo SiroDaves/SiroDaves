61c4620c106d814f9fde8d9b5c52aec18ae908b5
Siro Daves <sirodaves@Siros-MacBook-Pro.local>
2025-01-08 22:03:52 +0300

    react app

--- README.md
+++ README.md
@@ -1,14 +1,1 @@
 Hello there 👋, I am a Software engineer and a Technical Writer, Best at Flutter app development, full stack development with Mern. Other areas are like Android, .Net and Qt
-
-<!--<div>
-    <a href="https://github.com/JaxiroKe/github-readme-stats">
-        <img height=200
-            src="https://github-readme-stats-git-master-rstaa-rickstaa.vercel.app/api/top-langs/?username=JaxiroKe&layout=compact&langs_count=10&hide_border=1&role=OWNER,COLLABORATOR"
-            alt="My's Language stats" />
-    </a>
-    <a href="https://github.com/JaxiroKe/github-readme-stats">
-        <img height=200
-            src="https://github-readme-stats-git-master-rstaa-rickstaa.vercel.app/api?username=JaxiroKe&show_icons=true&count_private=true&line_height=28&hide_border=1&include_all_commits=true&card_width=450&role=OWNER,COLLABORATOR&exclude_repo=github-readme-stats"
-            alt="My's Github stats" />
-    </a>
-</div>-->
---
+++ app/layout.js
@@ -0,0 +1,33 @@
+import { GoogleTagManager } from "@next/third-parties/google";
+import { Inter } from "next/font/google";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./components/footer";
+import ScrollToTop from "./components/helper/scroll-to-top";
+import Navbar from "./components/navbar";
+import "./css/card.scss";
+import "./css/globals.scss";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Portfolio of Siro Daves - Software Eng",
+  description:
+    "Hi, there. My name is Siro. I am a skilled software developer with experience in a range of technologies including Flutter,  Android, .Net, ReactJS, Git, Databases, REST APIs among others. As part of various teams, I  have demonstrated my ability to work collaboratively towards common goals. I am an  effective communicator and a team player who is committed to meeting project deadlines  and ensuring successful project outcomes. I am a team player dedicated to delivering  innovative software solutions that meet the clients' needs.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ToastContainer />
+        <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
+          <Navbar />
+          {children}
+          <ScrollToTop />
+        </main>
+        <Footer />
+      </body>
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
+    </html>
+  );
+}
