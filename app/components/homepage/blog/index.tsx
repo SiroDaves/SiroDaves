76386cb22a5c46a33cf7ac086079b78610d33e93
Siro Daves <sirodevske@gmail.com>
2025-02-11 23:34:48 +0300

    Update index.tsx

--- app/components/homepage/blog/index.tsx
+++ app/components/homepage/blog/index.tsx
@@ -7,7 +7,7 @@ import { getHashnode } from '@/state/blog';
 async function BlogSection() {
   const blogs = await getHashnode();
   return (
-    <div id='blogs' className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
+    <div id='blog' className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
       <div className="w-[100px] h-[100px] bg-red-100 rounded-full absolute top-6 left-[42%] translate-x-1/2 filter blur-3xl  opacity-20"></div>
 
       <div className="flex justify-center -translate-y-[1px]">
